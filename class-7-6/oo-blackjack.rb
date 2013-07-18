5d64bc3b85d125dbc6535abb0d9aff5bd8909489
Gary Spagnoli <gary.spagnoli@gmail.com>
2013-07-17 23:33:42 -0400

    Latest Object Oriented Code
    Still not working properly, but can not find the error :\

--- class-7-6/oo-blackjack.rb
+++ class-7-6/oo-blackjack.rb
@@ -1,17 +1,36 @@
-# 1. Extract major nouns out of specifications
-# 2. Organize behaviors into the classes (Who does what?)
-# 3.
+require 'rubygems'
+require 'pry'
 
-class Card; 
-  attr_accessor :suit, :value
+# Object Oriented Blackjack Game
+# Combination of Class Notes and Solution Video
+# 1) Abstraction
+# 2) Encapsulation
+#
 
-  def initialize(value, suit)
-    self.suit = suit
-    self.value = value
+class Card
+  attr_accessor :suit, :face_value
+
+  def initialize(s, fv)
+    @suit = s
+    @face_value = fv
+  end
+
+  def pretty_output
+    "The #{face_value} of #{find_suit}"
   end
 
   def to_s
-    "#{value} of #{suit}"
+    pretty_output
+  end
+
+  def find_suit
+    ret_val = case suit
+                when 'H' then 'Hearts'
+                when 'D' then 'Diamonds'
+                when 'S' then 'Spades'
+                when 'C' then 'Clubs'
+              end
+    ret_val
   end
 end
 
@@ -19,31 +38,223 @@ class Deck
   attr_accessor :cards
 
   def initialize
-    self.cards = []
-    ['H', 'D', 'S', 'C'].product['1', '2', '3', '4', '5'].each do |arr|
-      self.cards << Card.new(arr[0], arr[1])
+    @cards = []
+    ['H', 'D', 'S', 'C'].each do |suit|
+      ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].each do |face_value|
+        @cards << Card.new(suit, face_value)
+      end
     end
+    scramble!
+  end
+
+  def scramble!
+    cards.shuffle!
+  end
+
+  def deal_one
+    cards.pop
+  end
+
+  def size
+    cards.size
   end
 end
 
-class Hand; end
-class Dealer; end
-class Player; end
+module Hand
+  def show_hand
+    puts "---- #{name}'s Hand ----"
+    cards.each do|card|
+      puts "=> #{card}"
+    end
+    puts "=> Total: #{total}"
+  end
+
+  def total
+    face_values = cards.map{|card| card.face_value }
+
+    total = 0
+    face_values.each do |val|
+      if val == "A"
+        total += 11
+      else
+        total += (val.to_i == 0 ? 10 : val.to_i)
+      end
+    end
+
+    face_values.select{|val| val == "A"}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+
+  def add_card(new_card)
+    cards << new_card
+  end
+
+  def is_busted?
+    total > 21
+  end
+end
+
+class Player
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize(n)
+    @name = n
+    @cards = []
+  end
+
+  def show_flop
+    show_hand
+  end
+
+end
+
+class Dealer
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize
+    @name = "Dealer"
+    @cards = []
+  end
+
+  def show_flop
+    puts "----  Dealer's Hand ----"
+    puts "=> First card is hidden"
+    puts "=> Second card is #{cards[1]}"
+  end
+
+end
+
 
 class Blackjack
   attr_accessor :deck, :player, :dealer
 
   def initialize
     @deck = Deck.new
-    @player = Player.new("tom")
+    @player = Player.new("Player1")
+    @dealer = Dealer.new
   end
 
+  def set_player_name
+    puts "What's your name??"
+    player.name = gets.chomp
+  end
+
+  def deal_cards
+    player.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+    player.add_card(deck.deal_one)
+    dealer.add_card(deck.deal_one)
+  end
+
+  def show_flop
+    player.show_flop
+    dealer.show_flop
+  end
+
+  def blackjack_or_bust?(player_or_dealer)
+    if player_or_dealer.total == 21
+      if player_or_dealer.is_a?(Dealer)
+        puts "Sorry, dealer hit blackjack. #{player.name} loses."
+      else
+        puts "Congratulations, you hit Blackjack! #{player.name} win!"
+      end
+      play_again?
+    elsif player_or_dealer.is_busted?
+      if player_or_dealer.is_a?(Dealer)
+        puts "Congratulations, dealer busted. {player.name} win!"
+      else
+        puts "Sorry, #{player.name} busted. #{player.name} loses."
+      end
+      play_again?
+    end
+  end
+
+  def player_turn
+    puts "#{player.name}'s turn."
+
+    blackjack_or_bust?(player)
+
+    while !player.is_busted?
+      puts "What would you like to do? 1) hit 2) stay"
+      response = gets.chomp
+
+      if !['1', '2',].include?(response)
+        puts "Error: you must enter 1 or 2"
+      next
+      end
+
+      if response == '2'
+        puts "#{player.name} chose to stay."
+        break
+      end
+
+      #hit
+      new_card = deck.deal_one
+      puts "Dealing card to #{player.name}: #{new_card}"
+      players.add_card(new_card)
+      puts "#{player.name}'s total is now: #{player.total}"
+
+      black_or_bust?(player)
+    end
+    puts "#{player.name} stays with #{player.total}"
+  end
+
+def dealer_turn
+  puts "Dealer's turn."
+
+  black_or_bust?(dealer)
+  while dealer.total < 17
+    new_card = deck.deal_one
+    puts "Dealing card to dealer: #{new_card}"
+    dealer.add_card(new_card)
+    puts "Dealer total is now #{dealer.total}"
+
+    black_or_bust?(dealer)
+  end
+  puts "Dealer stays with #{dealer.total}."
+end
+
+def who_won?
+  if player.total > dealer.total
+    puts "Congratulations, #{player.name} wins!"
+  elsif player.total < dealer.total
+    puts "Sorry, #{player.name} loses."
+  else
+    puts "It's a tie!"
+  end
+  play_again?
+end
+
+def play_again?
+  puts ""
+  puts "Would you like to play again? 1) yes 2) no, exit"
+  if gets.chomp = '1'
+    puts "Starting new game..."
+    puts ""
+    deck = Deck.new
+    player.cards = []
+    dealer.cards = []
+    start
+  else
+    puts "Goodbye!"
+    exit
+
   def start
+    set_player_name
     deal_cards
+    show_flop
     player_turn
     dealer_turn
-    compare_who_won
-    play_again?
+    who_won?(player, dealer)
+    play_again
   end
 
   def deal_cards
@@ -53,6 +264,60 @@ end
 game = Blackjack.new
 game.start
 
+player = Player.new("Chris")
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.show_hand
 
-card = Card.new('2', 'Spades')
-puts "my card is #{card}"
+dealer = Dealer.new
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.show_hand
+
+
+
+
+# ***Is a relationship => Inheritance***
+#
+# Entities that are sub categories of each other
+# class Animal
+#   def speak
+#     puts "from the Animal"
+#   end
+#
+#  def smell
+#
+#  end
+# end
+#
+# class Dog < Animal
+#   include Swimmable
+# end
+#
+# class Cat < Animal
+#   def speak
+#     puts 'meow'
+#   end
+# end
+
+
+# cat1 = Cat.new
+# cat1.speak
+# cat1.swim
+
+# dog1.dog.new
+# dog1.speak
+# dog1.swim
+
+
+# ***Has a relationship => Composition***
+# module Swimmable
+#   def swim
+#     puts "I'm swimming!"
+#   end
+# end
+
